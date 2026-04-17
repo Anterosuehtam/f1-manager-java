4fef5c7ae2f3995d7dee9b836b0087c1ed6ed644
Matheus Antero <101403401+Anterosuehtam@users.noreply.github.com>
2026-04-16 21:15:58 -0300

    feat: adiciona cria tabelas no banco de dados

--- src/main/java/com/br/f1_manager/model/Equipe.java
+++ src/main/java/com/br/f1_manager/model/Equipe.java
@@ -1,51 +1,30 @@
 package com.br.f1_manager.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "equipes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Equipe {
 
+    @Id
+    @Column(name = "id_equipe", length = 50)
     private String id;
 
+    @Column(nullable = false)
     private String nome;
 
     private String nacionalidade;
-
-
-    public Equipe() {
-
-    }
-
-    public Equipe(String id, String nome, String nacionalidade) {
-        this.id = id;
-        this.nome = nome;
-        this.nacionalidade = nacionalidade;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNacionalidade() {
-        return nacionalidade;
-    }
-
-    public void setNacionalidade(String nacionalidade) {
-        this.nacionalidade = nacionalidade;
-    }
-
-    @Override
-    public String toString() {
-        return "[" + id + "] " + nome  + " (" + nacionalidade + ")";
-    }
 
 }
